f6a47c4c5925f9b21ae48b691b5049fa0f978516
Nauryzbai <nauzik@mail.ru>
2019-04-03 17:48:24 +0600

    Практика1

---
+++ Practice1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mashimov Nauryzbai");
+            Console.WriteLine("Введите числа A,B:");
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine("A + B = " + (numberOne+numberTwo));
+            Console.ReadLine();
+        }
+    }
+}
